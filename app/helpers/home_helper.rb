630706d4e5b3239224da3d8f350bde7d77e9596b
Sebastian Vizcaino <elvizcacho@gmail.com>
2014-09-16 19:41:28 -0500

    init

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -21,4 +21,34 @@ module HomeHelper
 
 		return html.html_safe
 	end
+
+	def carrousel_tag(p)
+		i = 0
+		html = '<div id="myCarousel" class="carousel slide" data-ride="carousel">'
+		html += '<ol class="carousel-indicators">'
+		while i < p[:images].length
+			html += '<li data-target="#myCarousel" data-slide-to="'"#{i}"'" class="' + "#{(i == 0)? "active":""}" + '"></li>'
+			i += 1
+		end
+		html += '</ol>'
+		html += '<div class="carousel-inner">'
+		i = 0
+		while i < p[:images].length
+			html += '<div class="item' + "#{(i == 0)? " active":""}" +'">'
+			html += image_tag("#{p[:images][i]}")
+			html += '<div class="container">'
+			html += '<div class="carousel-caption">'
+			html += '<h1>' + "#{p[:titles][i]}" + '</h1>'
+			html += '<p>' + "#{p[:texts][i]}" + '</p>'
+			html += '</div>'
+			html += '</div>'
+			html += '</div>'
+			i += 1
+		end
+		html += '</div>'
+		html += '<a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>'
+		html += '<a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>'
+		html += '</div>'
+		return html.html_safe
+	end
 end
