bb6c39f05bcf919ef2686140abbfab134af12ddc
Sebastian Vizcaino <elvizcacho@gmail.com>
2014-11-09 13:43:37 -0500

    I've finished the translation of Tuix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
-	before_filter :asignar_idioma
+	before_filter :set_locale
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def asignar_idioma
-  	I18n.locale = params[:locale] || I18n.default_locale
+  def set_locale
+    #render :text => t('home.meta.description')
+  	if params[:locale]
+  		I18n.locale = params[:locale]
+  	elsif extract_locale_from_accept_language_header == 'es' 
+  		I18n.locale = 'es'
+  	elsif extract_locale_from_accept_language_header == 'en'
+  		I18n.locale = 'en'
+  	else
+  		I18n.locale = I18n.default_locale
+  	end
+  end
+ 
+private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def meta_tags(hash)
+		html = ''
+		html += '<meta property="title" content="' + hash[:title] + '" >'
+		html += '<meta name="description" content="' + hash[:description] +'" >'
+		html += '<meta property="og:title" content="' + hash[:og][:title] + '" >'
+		html += '<meta property="og:description" content="' + hash[:og][:description] + '" >'
+		html += '<meta property="og:url" content="' + hash[:og][:url] + '" >'
+		html += '<meta property="og:image" content="' + hash[:og][:image][:url] + '" >'
+		html += '<meta property="og:image:type" content="' + hash[:og][:image][:type] + '" >'
+		html += '<meta property="og:image:width" content="' + hash[:og][:image][:width] + '" >'
+		html += '<meta property="og:image:height" content="' + hash[:og][:image][:height] + '" >'
+		html += '<meta property="og:locale" content="' + hash[:og][:locale][:main] + '" >'
+		html += '<meta property="og:locale:alternate" content="' + hash[:og][:locale][:alternate] + '" >'
+		html.html_safe
+
+	end
 end
